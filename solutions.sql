e76e345e8a59e3843351d15adc0096e2f4494a41
eternalsphere25 <eternalsphere25@gmail.com>
2022-09-09 13:54:28 +0900

    solutions for 8+ and 9-

--- solutions.sql
+++ solutions.sql
@@ -1,6 +1,6 @@
 /*
 1: SELECT name
-https://sqlzoo.net/wiki/SELECT_from_WORLD_Tutorial
+https://sqlzoo.net/wiki/SELECT_names
 */
 
 -- 1. Find the country that start with Y
@@ -706,6 +706,136 @@ WHEN dept = 2 THEN 'Sci'
 WHEN dept = 3 THEN 'Art'
 ELSE 'None' END AS subject
 FROM teacher
+
+
+
+/*
+8+: Numeric Examples
+https://sqlzoo.net/wiki/NSS_Tutorial
+*/
+
+-- 1. Show the the percentage who STRONGLY AGREE
+SELECT A_STRONGLY_AGREE
+FROM nss
+WHERE question = 'Q01'
+AND institution = 'Edinburgh Napier University'
+AND subject = '(8) Computer Science'
+
+-- 2. Show the institution and subject where the score is at least 100 for question 15. 
+SELECT institution, subject
+FROM nss
+WHERE question = 'Q15'
+AND score >= 100
+
+-- 3. Show the institution and score where the score for '(8) Computer Science' is less than 50 for question 'Q15' 
+SELECT institution, score
+FROM nss
+WHERE question = 'Q15'
+AND subject = '(8) Computer Science'
+AND score < 50
+
+-- 4. Show the subject and total number of students who responded to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'. 
+SELECT subject, SUM(response)
+FROM nss
+WHERE question = 'Q22'
+AND (subject = '(8) Computer Science'
+     OR subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 5. Show the subject and total number of students who A_STRONGLY_AGREE to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+SELECT subject, SUM((A_STRONGLY_AGREE*response)/100) AS total
+FROM nss
+WHERE question = 'Q22'
+AND (subject = '(8) Computer Science'
+     OR subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 6. Show the percentage of students who A_STRONGLY_AGREE to question 22 for the subject '(8) Computer Science' show the same figure for the subject '(H) Creative Arts and Design'. 
+SELECT subject, ROUND((SUM(A_STRONGLY_AGREE*response)/SUM(response)), 0) AS pct
+FROM nss
+WHERE question = 'Q22'
+AND (subject = '(8) Computer Science'
+     OR subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+SELECT subject, ROUND(FLOOR((SUM((A_STRONGLY_AGREE*response)/100)CAST(SUM(response) AS float))*100),0)
+FROM nss
+WHERE (question = 'Q22' AND subject = '(8) Computer Science')
+OR (question = 'Q22' AND subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 7. Show the average scores for question 'Q22' for each institution that include 'Manchester' in the name.
+SELECT institution, ROUND(FLOOR(((SUM((score*response)/100)/CAST(SUM(response) AS float))*100)+0.3),0) AS average_score
+FROM nss
+WHERE question = 'Q22'
+AND institution LIKE '%Manchester%'
+GROUP BY institution
+
+-- 8. Show the institution, the total sample size and the number of computing students for institutions in Manchester for 'Q01'. 
+SELECT institution, SUM(sample) AS total_sample,
+     (SELECT sample
+      FROM nss T2
+      WHERE T1.instituion = T2.institution
+      AND question = 'Q01'
+      AND subject = '(8) Computer Science') AS computing
+FROM nss T1
+WHERE question = 'Q01'
+AND institution LIKE '%Manchester%'
+GROUP BY institution
+
+
+
+/*
+9-: Window functions
+https://sqlzoo.net/wiki/Window_functions
+*/
+
+-- 1. Show the lastName, party and votes for the constituency 'S14000024' in 2017. 
+SELECT lastName, party, votes
+FROM ge
+WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC
+
+-- 2. Show the party and RANK for constituency S14000024 in 2017. List the output by party
+SELECT party, votes, RANK() OVER (ORDER BY votes DESC) AS posn
+FROM ge
+WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party
+
+-- 3. Use PARTITION to show the ranking of each party in S14000021 in each year. Include yr, party, votes and ranking (the party with the most votes is 1).
+SELECT yr, party, votes, RANK() OVER (PARTITION BY yr ORDER BY votes DESC) AS posn
+FROM ge
+WHERE constituency = 'S14000021'
+ORDER BY party, yr
+
+-- 4. Use PARTITION BY constituency to show the ranking of each party in Edinburgh in 2017. Order your results so the winners are shown first, then ordered by constituency.
+SELECT sonstituency, party, votes, RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) AS posn
+FROM ge
+WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+AND yr = 2017
+ORDER BY posn, constituency
+
+-- 5. Show the parties that won for each Edinburgh constituency in 2017.
+SELECT constituency, party
+FROM ge T1
+WHERE votes >= ALL (SELECT votes
+                    FROM ge T2
+                    WHERE T2.constituency = T1.constituency
+                    AND yr = 2017)
+AND constituency BETWEEN 'S14000021' AND 'S14000026'
+AND yr = 2017
+ORDER BY constituency
+
+-- 6. Show how many seats for each party in Scotland in 2017.
+SELECT party, COUNT(party) AS num_seats
+FROM ge T1
+WHERE votes >= ALL (SELECT votes
+                    FROM ge T2
+                    WHERE T2.constituency = T1.constituency
+                    AND yr = 2017)
+AND constituency LIKE 'S%'
+AND yr = 2017
+GROUP BY (party)
 
 
 
